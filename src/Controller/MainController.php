cb8b19cc4e68aaf5599f38705023f559f3dca641
Gwigzz <jhonguetter199@hotmail.fr>
2022-03-31 19:09:44 +0200

    update blog

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,11 +2,6 @@
 
 namespace App\Controller;
 
-use App\Entity\Post;
-use App\Form\PostFormType;
-use App\Repository\PostRepository;
-use Doctrine\Persistence\ManagerRegistry;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -31,67 +26,5 @@ class MainController extends AbstractController
     public function contact(): Response
     {
         return $this->render('main/contact.html.twig', []);
-    }
-
-    /**
-     * Affiche les articles
-     */
-    #[Route('/blog', name: 'blog')]
-    public function blog(PostRepository $postRepository)
-    {
-        $showAllArticle = $postRepository->findAll();
-
-        // Recherche du contenue depuis l'entité par le "repository"
-        // $article = $postRepository->findOneByContent('dsf');
-
-        return $this->render(
-            'main/blog.html.twig',
-            [
-                'articles' => $showAllArticle,
-            ]
-        );
-    }
-
-    /**
-     * Page ajouter un nouvel article
-     */
-    #[Route('/creer_un_article', name: 'new_post')]
-    public function newArticle(Request $request, PostRepository $postRepository): Response
-    {
-
-        $postEntity = new Post();
-        $form = $this->createForm(PostFormType::class, $postEntity);
-
-        // On récupère la requète via $request
-        $form->handleRequest($request);
-
-        // Gestion de la soumission du formulaire
-        if ($form->isSubmitted() && $form->isValid()) {
-
-            // On insert en base de donnée via notre repository
-            $postRepository->add($postEntity);
-
-            // Flash message
-            $this->addFlash('success', 'publication ajouté');
-
-            /* Autre moyen d'insérrer en bdd */
-            // Injecter (ManagerRegistry $doctrine) au seins de la methode
-            // $entityManager = $doctrine->getManager();
-            // // Le "persist" permet de s'adapater à l'entité demandée
-            // $entityManager->persist($form->getData());
-            // // Déclanche l'action en base de donnée
-            // $entityManager->flush();
-
-            // Retourne la réponse si "submit" & "valide"
-            return $this->redirectToRoute('app_main_blog');
-        }
-
-
-        return $this->renderForm(
-            'main/new_article.html.twig',
-            [
-                'form_post' => $form
-            ]
-        );
     }
 }
---
+++ src/Controller/PostController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Post;
+use App\Form\PostType;
+use App\Repository\PostRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/post')]
+class PostController extends AbstractController
+{
+    /**
+     * Page principale
+     */
+    #[Route('/', name: 'app_post_index', methods: ['GET'])]
+    public function index(PostRepository $postRepository): Response
+    {
+        return $this->render('post/index.html.twig', [
+            'posts' => $postRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * Creer une publication
+     */
+    #[Route('/new', name: 'app_post_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, PostRepository $postRepository): Response
+    {
+        $post = new Post();
+        $form = $this->createForm(PostType::class, $post);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $postRepository->add($post);
+
+            $this->addFlash('success', 'publication Ajouté');
+
+            return $this->redirectToRoute('app_post_index', []);
+        }
+
+        return $this->renderForm('post/new.html.twig', [
+            'post' => $post,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * Voir une publication
+     */
+    #[Route('/{id}', name: 'app_post_show', methods: ['GET'])]
+    public function show(Post $post): Response
+    {
+        return $this->render('post/show.html.twig', [
+            'post' => $post,
+        ]);
+    }
+
+    /**
+     * Editer un article
+     */
+    #[Route('/{id}/edit', name: 'app_post_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Post $post, PostRepository $postRepository): Response
+    {
+        $form = $this->createForm(PostType::class, $post);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $postRepository->add($post);
+
+            $this->addFlash('success', 'publication Modifié');
+
+            return $this->redirectToRoute('app_post_index', []);
+        }
+
+        return $this->renderForm('post/edit.html.twig', [
+            'post' => $post,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * Supprésion d'un article
+     */
+    #[Route('/{id}', name: 'app_post_delete', methods: ['POST'])]
+    public function delete(Request $request, Post $post, PostRepository $postRepository): Response
+    {
+        if ($this->isCsrfTokenValid('mon_joli_block' . $post->getId(), $request->request->get('_token'))) {
+            $postRepository->remove($post);
+            $this->addFlash('success', 'publication Supprimé');
+        }
+
+        return $this->redirectToRoute('app_post_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
