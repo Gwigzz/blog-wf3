be28dbdb84e5f9e00aeec73596f23271739882d9
Gwigzz <jhonguetter199@hotmail.fr>
2022-04-01 14:49:38 +0200

    login

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 
 #[Route('/', name: 'app_main_')]
@@ -26,5 +27,33 @@ class MainController extends AbstractController
     public function contact(): Response
     {
         return $this->render('main/contact.html.twig', []);
+    }
+
+    /**
+     * Connexion
+     */
+    #[Route(path: '/connexion', name: 'login')]
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        // Si utilisateur est "log", on redirige sur "home"
+        if ($this->getUser()) {
+            return $this->redirectToRoute('app_main_home');
+        }
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+    /**
+     * Déconnexion
+     */
+    #[Route(path: '/deconnexion', name: 'logout')]
+    public function logout(): void
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 }
