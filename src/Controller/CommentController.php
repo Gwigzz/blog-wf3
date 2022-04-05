41963113a6de6b410d37e82ce59118e42293c9f3
Gwigzz <jhonguetter199@hotmail.fr>
2022-04-05 09:51:07 +0200

    update 6.0.7

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -5,10 +5,10 @@ namespace App\Controller;
 use App\Entity\Comment;
 use App\Form\CommentType;
 use App\Repository\CommentRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/comment')]
 class CommentController extends AbstractController
